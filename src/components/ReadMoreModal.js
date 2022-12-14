d007f43a5ec8c98d4e8c60fe6e82323f1d110aa2
Kurt Donaldson <kurt_donaldson@hotmail.com>
2022-12-15 09:06:05 +1300

    tab logos added

--- src/components/ReadMoreModal.js
+++ src/components/ReadMoreModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   MDBBtn,
   MDBModal,
@@ -8,31 +8,36 @@ import {
   MDBModalTitle,
   MDBModalBody,
   MDBModalFooter,
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 import "../components/readMoreModal.css";
 
-export default function ReadMoreModal({title, content}) {
+export default function ReadMoreModal({ title, content }) {
   const [basicModal, setBasicModal] = useState(false);
 
   const toggleShow = () => setBasicModal(!basicModal);
 
   return (
     <>
-      <MDBBtn onClick={toggleShow} className="modal-button">{title}</MDBBtn>
-      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex='-1'>
+      <MDBBtn onClick={toggleShow} className="modal-button">
+        READ MORE
+      </MDBBtn>
+      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
         <MDBModalDialog>
           <MDBModalContent>
             <MDBModalHeader>
               <MDBModalTitle>{title}</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
             </MDBModalHeader>
-            <MDBModalBody className='modal-content'>{content}</MDBModalBody>
+            <MDBModalBody className="modal-content">{content}</MDBModalBody>
 
             <MDBModalFooter>
-              <MDBBtn color='secondary' onClick={toggleShow}>
+              <MDBBtn color="secondary" onClick={toggleShow}>
                 Close
               </MDBBtn>
-        
             </MDBModalFooter>
           </MDBModalContent>
         </MDBModalDialog>
