60967c02dbe4a2095b49ec087978aac98c7cdf4c
Kurt Donaldson <kurt_donaldson@hotmail.com>
2022-12-16 10:40:25 +1300

    about page responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,23 +9,25 @@ function About() {
         <h1>
           Limitless <span>Physiotherapy</span>
         </h1>
-        <p>
+        <p className="w-75">
           At Limitless Physiotherapy we are dedicated to providing top quality
           physiotherapy treatment in order to help people reach their functional
           goals.
         </p>
-        <a
-          className="book-button"
-          type="button"
-          href="https://nzappts.gensolve.com/limitless"
-          target="_blank"
-          rel="noreferrer"
-        >
-          BOOK ONLINE
-        </a>
+        <div className="d-flex justify-content-center">
+          <a
+            className="book-button"
+            type="button"
+            href="https://nzappts.gensolve.com/limitless"
+            target="_blank"
+            rel="noreferrer"
+          >
+            BOOK ONLINE
+          </a>
+        </div>
       </header>
-      <main className="img_caption_div d-flex justify-content-start">
-        <div className="profile_pic w-50">
+      <main className="img_caption_div d-flex flex-column align-items-center flex-md-row justify-content-md-center align-items-md-start">
+        <div className="profile_pic">
           <img src={ProfilePic} alt="" />
           <figcaption>
             <p>
@@ -41,36 +43,41 @@ function About() {
             </ul>
           </figcaption>
         </div>
-        <div className="profile_blurb w-50">
+        <div className="profile_blurb">
           <div>
-            <h2>DIRECTOR OF LIMITLESS PHYSIOTHERAPY</h2>
-            <p>
-              Kurt completed his Bachelor of Physiotherapy at the University of
-              Technology (AUT).
-            </p>
-            <p>
-              He has gone on to complete various post graduate training courses
-              and is currently completing a Masters of Musculoskeletal
-              Physiotherapy at AUT.
-            </p>
-            <p>
-              Kurt is focused on keeping updated with the latest evidence based
-              research in order to provide people with the highest quality
-              service.
-            </p>
-            <p>
-              Kurt is passionate about providing quality physiotherapy services
-              including carrying out a combination of manual therapy techniques
-              and gym based functional rehabilitation.
-            </p>
-            <p>
-              He has acquired a wide range of experience in New Zealand and
-              overseas treating various musculoskeletal conditions and injuries.
-            </p>
-            <p>
-              He has also been the team physiotherapist for several sports teams
-              including various rugby and ruby league teams.
-            </p>
+            <div>
+              <h2>DIRECTOR OF LIMITLESS PHYSIOTHERAPY</h2>
+            </div>
+            <div>
+              <p>
+                Kurt completed a Bachelor of Health Science (Physiotherapy) at
+                Auckland University of Technology (AUT) in 2014.
+              </p>
+              <p>
+                He has gone on to complete various post graduate training
+                courses and is currently completing a Masters of Musculoskeletal
+                Physiotherapy at AUT.
+              </p>
+              <p>
+                Kurt is focused on keeping updated with the latest evidence
+                based research in order to provide people with the highest
+                quality service.
+              </p>
+              <p>
+                Kurt is passionate about providing quality physiotherapy
+                services including carrying out a combination of manual therapy
+                techniques and gym based functional rehabilitation.
+              </p>
+              <p>
+                He has acquired a wide range of experience in New Zealand and
+                overseas treating various musculoskeletal conditions and
+                injuries.
+              </p>
+              <p>
+                He has also been the team physiotherapist for several sports
+                teams including various rugby and ruby league teams.
+              </p>
+            </div>
           </div>
         </div>
       </main>
