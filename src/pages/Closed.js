80c587e34cd6c4ff21e64034cbec63c02251e870
kurtdonaldson <89885417+kurtdonaldson@users.noreply.github.com>
2024-02-08 07:27:42 +1300

    align text

--- src/pages/Closed.js
+++ src/pages/Closed.js
@@ -3,7 +3,7 @@ import React from "react";
 const Closed = () => {
   return (
     <div
-      style={{ height: "100vh", width: "100vw" }}
+      style={{ height: "100vh", width: "100vw", textAlign: "center" }}
       className="d-flex flex-column justify-content-center h-100 align-items-center"
     >
       <p>Limitless Physiotherapy is now closed</p>
